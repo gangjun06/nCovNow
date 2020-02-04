750df9d3d72f2a7ba1c2ba7a4c72bb9deca0673c
GangjunLee <me@gangjun.dev>
2020-02-04 22:05:06 +0900

    update favicon.ico & add credit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,39 @@
 <template>
   <v-footer padless class="indigo lighten-1 white--text">
-    <v-col class="text-center "  cols="12">
-      &copy; {{ new Date().getFullYear() }} — <strong><a href="https://gangjun.dev" target="_blank">Gangjun </a></strong>
+    <v-col class="text-center " cols="12">
+      &copy; {{ new Date().getFullYear() }} —
+      <strong
+        ><a href="https://gangjun.dev" class="simpleLink" target="_blank">Gangjun </a></strong
+      >
+      <v-btn text color="white" @click="creditOverlay = !creditOverlay">
+        Credit.
+      </v-btn>
+      <v-overlay :value="creditOverlay">
+        <div>favicon : <a class="simpleLink" href="https://www.flaticon.com/authors/smashicons" target="_blank">flaticon - Smashicons</a></div>
+        <v-btn icon @click="creditOverlay = false">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-overlay>
     </v-col>
   </v-footer>
 </template>
 
+<script>
+export default {
+  data: () => ({
+    creditOverlay: false
+  })
+}
+</script>
+
 <style>
- strong a{
-  text-decoration: none;
-  color:white !important;
+.simpleLink {
+  text-decoration: white;
+  color: white !important;
   -webkit-transition: color 0.5s;
   transition: color 0.5s;
 }
-strong a:hover{
-  color:#efefef !important;
+.simpleLink:hover {
+  color: #cdcdcd !important;
 }
 </style>
